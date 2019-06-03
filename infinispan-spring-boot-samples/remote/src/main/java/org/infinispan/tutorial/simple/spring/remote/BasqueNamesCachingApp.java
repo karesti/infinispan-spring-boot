6625d7f407dc581b83119f3c4d409cfcfe63b503
Katia Aresti <karesti@redhat.com>
2019-06-03 08:41:56 +0200

    wip

--- infinispan-spring-boot-samples/remote/src/main/java/org/infinispan/tutorial/simple/spring/remote/BasqueNamesCachingApp.java
+++ infinispan-spring-boot-samples/remote/src/main/java/org/infinispan/tutorial/simple/spring/remote/BasqueNamesCachingApp.java
@@ -10,7 +10,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 public class BasqueNamesCachingApp {
 
-   public static void main(String... args) {
-      new SpringApplicationBuilder().sources(BasqueNamesCachingApp.class).run(args);
-   }
+    public static void main(String... args) {
+        new SpringApplicationBuilder().sources(BasqueNamesCachingApp.class).run(args);
+    }
 }
--- infinispan-spring-boot-starter-remote/src/test/java/test/org/infinispan/spring/starter/remote/actuator/RemoteCacheMetricBinderTest.java
+++ infinispan-spring-boot-starter-remote/src/test/java/test/org/infinispan/spring/starter/remote/actuator/RemoteCacheMetricBinderTest.java
@@ -3,51 +3,53 @@ package test.org.infinispan.spring.starter.remote.actuator;
 import static java.util.Collections.emptyList;
 
 import org.infinispan.client.hotrod.RemoteCache;
-import org.infinispan.client.hotrod.RemoteCacheManager;
 import org.infinispan.spring.common.provider.SpringCache;
 import org.infinispan.spring.starter.remote.actuator.RemoteInfinispanCacheMeterBinderProvider;
+import org.infinispan.test.jupiter.InfinispanServerExtension;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
+import org.junit.jupiter.api.BeforeAll;
 
 import io.micrometer.core.instrument.binder.cache.CacheMeterBinder;
 import io.micrometer.core.instrument.binder.cache.CacheMeterBinderCompatibilityKit;
-import test.org.infinispan.spring.starter.remote.container.InfinispanContainer;
 
-@Testcontainers
 public class RemoteCacheMetricBinderTest extends CacheMeterBinderCompatibilityKit {
-   @Container
-   private final static InfinispanContainer INFINISPAN_SERVER = new InfinispanContainer().withCache("mycache");
 
-   private static RemoteCacheManager cacheManager;
-   private RemoteCache<String, String> cache;
+    private static InfinispanServerExtension infinispanServerExtension = InfinispanServerExtension.builder()
+            .withCaches("mycache").build();
 
-   @AfterAll
-   public static void cleanup() {
-      INFINISPAN_SERVER.stop();
-   }
+    private RemoteCache<String, String> cache;
 
-   @AfterEach
-   public void cleanCache() {
-      cache.clientStatistics().resetStatistics();
-   }
+    @AfterEach
+    public void cleanCache() {
+        cache.clientStatistics().resetStatistics();
+    }
 
-   @Override
-   public CacheMeterBinder binder() {
-      cacheManager = INFINISPAN_SERVER.getCacheManager();
-      cache = cacheManager.getCache("mycache");
-      RemoteInfinispanCacheMeterBinderProvider remoteInfinispanCacheMeterBinderProvider = new RemoteInfinispanCacheMeterBinderProvider();
-      return (CacheMeterBinder) remoteInfinispanCacheMeterBinderProvider.getMeterBinder(new SpringCache(cache), emptyList());
-   }
+    @BeforeAll
+    public static void start() {
+        infinispanServerExtension.start();
+    }
 
-   @Override
-   public void put(String key, String value) {
-      cache.put(key, value);
-   }
+    @AfterAll
+    public static void stop() {
+        infinispanServerExtension.stop();
+    }
 
-   @Override
-   public String get(String key) {
-      return cache.get(key);
-   }
+    @Override
+    public CacheMeterBinder binder() {
+        cache = infinispanServerExtension.hotRodClient().getCache("mycache");
+        RemoteInfinispanCacheMeterBinderProvider remoteInfinispanCacheMeterBinderProvider = new RemoteInfinispanCacheMeterBinderProvider();
+        return (CacheMeterBinder) remoteInfinispanCacheMeterBinderProvider
+                .getMeterBinder(new SpringCache(cache), emptyList());
+    }
+
+    @Override
+    public void put(String key, String value) {
+        cache.put(key, value);
+    }
+
+    @Override
+    public String get(String key) {
+        return cache.get(key);
+    }
 }
